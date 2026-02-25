bd66e3aa6ab8e2fb2162ca0be069b0a62c3e9e92
GManaswi2026 <gmanaswi9@gmail.com>
2026-02-25 12:54:26 +0000

    print n natural numbers

---
+++ practice_question/q7.cpp
@@ -0,0 +1,17 @@
+//print n natural numbers
+
+#include<iostream>
+using namespace std;
+int main(){
+  
+    for(int i=0;i<=5;i++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+    int n =15;
+    for(int i=0;i<n;i++){
+        cout<<i<<"  ";
+    }
+    cout<<endl;
+    return 0;
+}
