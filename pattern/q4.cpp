614743cd1edfdab645141cb4de47727ca07e46f5
GManaswi2026 <gmanaswi9@gmail.com>
2026-03-22 17:20:30 +0000

    floyd triangle pattern

---
+++ pattern/q4.cpp
@@ -0,0 +1,19 @@
+//floyd triangle pattern
+//1     
+//2 3
+//4 5 6
+//7 8 9 10
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n =4;
+    int count =1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<count<<" ";
+            count++;
+        }
+        cout<<endl;
+    }
+}
