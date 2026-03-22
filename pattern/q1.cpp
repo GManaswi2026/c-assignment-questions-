ce7762f7e3577d68d0c9cfcf124847b82533aa6e
GManaswi2026 <gmanaswi9@gmail.com>
2026-03-22 17:19:25 +0000

    hollow rect pattern

---
+++ pattern/q1.cpp
@@ -0,0 +1,21 @@
+//print hollow rectangle pattern
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    for(int i =1;i<=n;i++){
+           cout<<" * "<<" ";
+
+           for(int j=1;j<=5;j++){
+               if(i==1 || i==n){
+                   cout<<" * "<<" ";
+               }
+               else{
+                   cout<<"   "<<" ";
+               }
+           }
+           cout<<" * "<<" ";
+           cout<< endl;
+    }
+}
